1f11aefe30b005a14b9cd4f04ac46142d789e187
Kevin <1031158040@qq.com>
2021-06-07 12:01:21 +0800

    修改android变量

--- android/src/main/java/ui/fileselector/RNFileSelectorModule.java
+++ android/src/main/java/ui/fileselector/RNFileSelectorModule.java
@@ -5,9 +5,14 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+/* modified by kevin at 2021-06-07 begin */ 
+// import android.support.v4.app.ActivityCompat;
+// import android.support.v4.content.ContextCompat;
+// import android.support.v7.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
+/* modified by kevin at 2021-06-07 end */ 
 import android.util.Log;
 import android.widget.Toast;
 
